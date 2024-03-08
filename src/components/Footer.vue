076effbcd702cfa0c76bc68b445c3d43109cd988
ahmod001 <ahmodh08@gmail.com>
2024-03-08 18:47:26 +0600

    feature: footer added

---
+++ src/components/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+    <footer class="text-center py-3 bg-gray-100">
+        <span>
+            Made with ❤️ by <a
+                href="https://github.com/ahmod001"
+                target="_blank" class="font-medium text-gray-700">Ahmod Hasan</a>
+        </span>
+    </footer>
+</template>
